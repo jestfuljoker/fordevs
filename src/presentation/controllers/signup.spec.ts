dfb6175bedbd467f4e13f23fecc083c98f2ba380
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-05 02:50:11 -0300

    test: ensure SignUpController returns message if name is not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,5 +15,6 @@ describe('SignUpController', () => {
 		const httpResponse = sut.handle(httpRequest);
 
 		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new Error('Missing param: name'));
 	});
 });
