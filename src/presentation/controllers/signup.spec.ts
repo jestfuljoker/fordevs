c5bc0c34e5d51175e4fca963a78033070d71868e
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-05 15:28:25 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -114,4 +114,23 @@ describe('SignUpController', () => {
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new InvalidParamError('email'));
 	});
+
+	it('should call EmailValidator with correct email', () => {
+		const { sut, emailValidatorStub } = makeSut();
+
+		const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+
+		const httpRequest = {
+			body: {
+				name: 'any_name',
+				email: 'any_email@mail.com',
+				password: 'any_password',
+				passwordConfirmation: 'any_password',
+			},
+		};
+
+		sut.handle(httpRequest);
+
+		expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
+	});
 });
