3bc1552714fc021e5f8987126fd5d337ff177972
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-09 07:18:30 -0300

    refactor: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -25,11 +25,13 @@ export class SignupController implements Controller {
 				}
 			}
 
-			if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+			const { email, password, passwordConfirmation } = httpRequest.body;
+
+			if (password !== passwordConfirmation) {
 				return badRequest(new InvalidParamError('passwordConfirmation'));
 			}
 
-			if (!this.emailValidator.isValid(httpRequest.body.email)) {
+			if (!this.emailValidator.isValid(email)) {
 				return badRequest(new InvalidParamError('email'));
 			}
 
