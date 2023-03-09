0aaa92a14c89bdb3504fc0ecc1b1101ce758d514
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-09 08:02:00 -0300

    test: ensure SignUpContoller returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -249,4 +249,27 @@ describe('SignUpController', () => {
 			password: 'any_password',
 		});
 	});
+
+	it('should return 200 if valid data is provided', () => {
+		const { sut } = makeSut();
+
+		const httpRequest = {
+			body: {
+				name: 'valid_name',
+				email: 'valid_email@mail.com',
+				password: 'valid_password',
+				passwordConfirmation: 'valid_password',
+			},
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(200);
+		expect(httpResponse.body).toEqual({
+			id: 'valid_id',
+			name: 'valid_name',
+			email: 'valid_email@mail.com',
+			password: 'valid_password',
+		});
+	});
 });
