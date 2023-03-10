db8daf7d7da32328ae331fb5dea0b7b00280b3d9
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-10 07:27:03 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	roots: ['<rootDir>/src'],
-	coverageProvider: 'v8',
-	collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/*.d.ts'],
 	coverageDirectory: 'coverage',
 	transform: {
 		'^.+\\.ts?$': '@swc/jest',
