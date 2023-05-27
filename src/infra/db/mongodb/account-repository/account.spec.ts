2ac62d2e4804b3aebfdc014842f52e4424c98c44
Christofer Assis <chris.f.assis18@gmial.com>
2023-05-27 18:28:23 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+
+import { MongoHelper } from '../helpers';
+import { AccountMongoRepository } from './account';
+
+describe('Account MongoDB Repository', () => {
+	beforeAll(async () => {
+		await MongoHelper.connect(process.env.MONGO_URL as string);
+	});
+
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+
+	it('should return an account on success', async () => {
+		const sut = new AccountMongoRepository();
+
+		const accountData = {
+			name: faker.name.fullName(),
+			email: faker.internet.email(),
+			password: faker.internet.password(),
+		};
+
+		const account = await sut.add(accountData);
+
+		expect(account).toBeTruthy();
+		expect(account.id).toBeTruthy();
+		expect(account.name).toBe(accountData.name);
+		expect(account.email).toBe(accountData.email);
+		expect(account.password).toBe(accountData.password);
+	});
+});
