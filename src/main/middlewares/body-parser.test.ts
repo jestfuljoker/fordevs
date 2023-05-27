8466afef22d791adb0ffcc1278ba66c8662e7f8e
Christofer Assis <chris.f.assis18@gmial.com>
2023-05-27 20:50:26 -0300

    test: ensure requests parse body as JSON

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+
+import { app } from '../config/app';
+
+describe('Body Parser Middleware', () => {
+	it('should parse body as JSON', async () => {
+		app.post('/test_body_parser', (request, response) => {
+			response.send(request.body);
+		});
+
+		await request(app)
+			.post('/test_body_parser')
+			.send({ name: 'Christofer' })
+			.expect({ name: 'Christofer' });
+	});
+});
