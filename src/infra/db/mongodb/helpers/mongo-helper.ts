96f3fadd00f5e2716c88a1abd11f4882f85682fd
Christofer Assis <chris.f.assis18@gmial.com>
2023-05-27 17:05:11 -0300

    feat: add mongodb methods helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+
+export class MongoHelper {
+	private static client: MongoClient;
+
+	private constructor() {}
+
+	static async connect(uri: string): Promise<void> {
+		this.client = await MongoClient.connect(uri);
+	}
+
+	static async disconnect(): Promise<void> {
+		await this.client.close();
+	}
+}
