3fe4a6a89f85a4903c559c2fa329fa2c317c81cd
Christofer Assis <chris.f.assis18@gmial.com>
2023-05-28 17:16:55 -0300

    refactor: connect to mongoDb before integration tests run

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,10 @@ module.exports = {
 	},
 	preset: '@shelf/jest-mongodb',
 	moduleNameMapper: {
-		'@main/(.*)': '<rootDir>/src/main/$1',
 		'@data/(.*)': '<rootDir>/src/data/$1',
 		'@domain/(.*)': '<rootDir>/src/domain/$1',
+		'@infra/(.*)': '<rootDir>/src/infra/$1',
+		'@main/(.*)': '<rootDir>/src/main/$1',
 		'@presentation/(.*)': '<rootDir>/src/presentation/$1',
 		'@utils/(.*)': '<rootDir>/src/utils/$1',
 	},
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,22 @@
 import request from 'supertest';
 
+import { MongoHelper } from '@infra/db/mongodb/helpers';
+
 import { app } from '../config/app';
 
 describe('Signup Routes', () => {
+	beforeAll(async () => {
+		await MongoHelper.connect(process.env.MONGO_URL as string);
+	});
+
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+
+	beforeEach(async () => {
+		await MongoHelper.getCollection('accounts').deleteMany({});
+	});
+
 	it('should return an account on success', async () => {
 		await request(app)
 			.post('/api/signup')
