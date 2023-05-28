8680567c6edfb6a5982ef2daa17b20df04b72ff7
Christofer Assis <chris.f.assis18@gmial.com>
2023-05-27 21:14:38 -0300

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+
+import { app } from '../config/app';
+
+describe('Cors Middleware', () => {
+	it('should enable cors', async () => {
+		app.get('/test_cors', (_, response) => {
+			response.send();
+		});
+
+		await request(app)
+			.get('/test_body_parser')
+			.expect('access-control-allow-origin', '*')
+			.expect('access-control-allow-methods', '*')
+			.expect('access-control-allow-headers', '*');
+	});
+});
